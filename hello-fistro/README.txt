d4b89eba8a9e8a59136b860a7f3f50e0c6e0422e
JuanKa Díaz <juan.diaz@jdevelopia.com>
2017-11-16 10:53:32 +0100

    First commit - v.0.0.1

---
+++ hello-fistro/README.txt
@@ -0,0 +1,31 @@
+=== Hello Fistro ===
+Contributors: juankadiaz
+Donate Link: https://jdevelopia.com/
+Tags: humor, comedy, chiquito
+Requires at least: 4.0
+Tested up to: 4.9
+Stable tag: 0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+There is a free and interpreted version based on Hello Dolly and reinterpreted with expresions of a great comedy actor of Spain, Chiquito de la Calzada, who died at December 11. This plugin is in her memory.
+
+== Description ==
+
+There is a free and interpreted version based on Hello Dolly and reinterpreted with expresions of a great comedy actor of Spain, Chiquito de la Calzada, who died at December 11. This plugin is in her memory.
+
+== Installation ==
+
+**Installation through the WordPress Dashboard**
+
+1. Login to your WordPress admin.
+2. Click on the plugins tab.
+3. Click the Add New button.
+4. Search for "Hello Fistro".
+5. Look for our plugin, click "Install Now" and then "Activate".
+6. Enjoy the great expressions of the master of comedy Chiquito de la Calzada.
+
+== Changelog ==
+
+= 0.0.1 (December 13, 2017) =
+* **First Release**. This is the first release of this plugin based on the original Hello Dolly.
---
+++ hello-fistro/hello-fistro.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @package Hello_Fistro
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Hello Fistro
+Plugin URI: htts://jdevelopia.com
+Description: Esto no es sólo un plugin, simboliza la esperanza, el humor y la cultura de un gran maestro cómico e icono de la cultura pop española, como es Chiquito de la Calzada.
+Author: JuanKa Díaz
+Version: 0.0.1
+Author URI: https://jdevelopia.com/
+*/
+
+function hello_fistro_get_lyric() {
+	/** These are the lyrics to Hello Fistro */
+	$lyrics = "¡Pecador!
+¿Te da cuen?
+En vez del graduado escolar tenía una etiqueta de Anís del Mono
+Trabaja menos que el sastre de Tarzán
+Eres más falso que el flequillo del Dioni
+Ere ma violento que el entrenador de lo Pogüer Renlle
+Tienes más pintura que el neceser de Marujita Díaz
+Sieteee caballo que vienennn de Bonanzaaarrlll
+¡Ten cuidadínnn no te hagas pupita en el fistro duodenalll!
+Eres más feo que el Fari comiendo limones
+Eres más peligroso que un tiroteo en un ascensor
+¡Fistro!
+¡Le voy a jasé un número nuevoooorl!
+¡Pecaor de la praderar!
+¡No puedor, no puedor!
+¡No te digo trigor por no llamarte Rodrigo!
+¡Asexsuarrr!
+¿Cómorr?
+¡Hombre malo, violento!
+¡Fuegorrrl!
+¡Aguaaa, aguaaa!
+¡Hijoputarrl!
+¡A can demor e narrr!
+¡Cobarderr!
+¡Al ataquerrrr!
+Meretérita
+Cantando: Sieteee caballo que vienennn de Bonanzaaarrlll
+¡Ten cuidadínnn no te hagas pupita en el fistro duodenalll!
+¡Jarl!
+¡Que lo sepas!
+¡Hijo míoorrr!
+¡Er putaaarrr!
+¡Hasta luego Lucasss!
+¡Por la gloria de mi madre!
+Tienes más pintura que el neceser de Marujita Díaz
+Eres más falso que el flequillo del Dioni
+Eres más feo que el Fari comiendo limones
+Eres más peligroso que un tiroteo en un ascensor
+¡Se ha pasao usted con mi bambina! ¡Es usted un torpedo del año cuatro!
+Ere ma violento que el entrenador de lo Pogüer Renlle
+Trabaja menos que el sastre de Tarzán
+Acapiporla
+Ay canemor
+A güán, a peich, agromenáuer
+¡Cuidadín!
+¡Te voy a borrar el cerito sesualrllr!
+¡Ese fistro danimarl!
+¡Te viá cortá el fiistro dabajo!
+¡Uno que nació después de los dolores!
+¡No puede sé!
+¡Es un diodená de persona humana!
+¡Un gesto de hombre malo de la pradera!
+Apiticain, mor nau
+Se mueve uhté mah que los precioh!
+¡Relájese usterl!
+Relájate físicamente, morálmente
+Lo maté en agosto, la calóh apretaba
+En vez del graduado escolar tenía una etiqueta de Anís del Mono
+¡No compre pan!";
+
+	// Split the texto into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// Choose a quote random
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+// Echoes the chosen random line
+function hello_fistro() {
+	$chosen = hello_fistro_get_lyric();
+	echo "<p id='fistro'>$chosen</p>";
+}
+
+// Execute the function
+add_action( 'admin_notices', 'hello_fistro' );
+
+// Styles in CSS in-line
+function fistro_css() {
+	// Positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	#fistro {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 13px;
+		font-style: italic;
+	}
+	</style>
+	";
+}
+add_action( 'admin_head', 'fistro_css' );
+?>
